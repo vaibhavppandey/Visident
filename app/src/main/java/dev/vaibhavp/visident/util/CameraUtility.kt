993538d511a5324694707285e77b014ee7c39291
vaibhavp <vaibhav.ppandey@proton.me>
2026-06-09 22:17:30 +0530

    refactor(camera): remove unused bindCameraUseCases from CameraUtility

--- app/src/main/java/dev/vaibhavp/visident/util/CameraUtility.kt
+++ app/src/main/java/dev/vaibhavp/visident/util/CameraUtility.kt
@@ -2,47 +2,15 @@ package dev.vaibhavp.visident.util
 
 import android.content.Context
 import android.net.Uri
-import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.lifecycle.awaitInstance
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LifecycleOwner
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.io.File
 
 object CameraUtility {
 
     private val timber = Timber.tag("CameraUtility")
-
-    suspend fun bindCameraUseCases(
-        context: Context,
-        lifecycleOwner: LifecycleOwner,
-        previewUseCase: Preview,
-        imageCaptureUseCase: ImageCapture,
-        cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-    ): Boolean {
-        return withContext(Dispatchers.Main) {
-            try {
-                val cameraProvider = ProcessCameraProvider.awaitInstance(context)
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    previewUseCase,
-                    imageCaptureUseCase
-                )
-                true
-            } catch (exc: Exception) {
-                timber.e(exc, "Use case binding failed")
-                false
-            }
-        }
-    }
 
     fun takePicture(
         context: Context,
